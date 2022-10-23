4883723b1720fd853d099bda308d27bf694b0b2c
Aura Velez <auraveleze@gmail.com>
2022-10-22 22:45:12 -0500

    Creacion de carpetas y mini ejemplo de prueba

---
+++ raptors-shop/src/Componentes/Menu.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../Estilos/Menu.css';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import IngresoProductos from '../Auraa/IngresoProductos';
+
+function Menu() {}
+
+export default IngresoProductos;
