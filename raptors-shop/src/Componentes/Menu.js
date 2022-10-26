e6d5c021c17ed9d972d6bf9ea6a2933269999000
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2022-10-25 22:27:07 -0500

    Borrado de todo

--- raptors-shop/src/Componentes/Menu.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../Estilos/Menu.css';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import IngresoProductos from '../Auraa/IngresoProductos';
-
-function Menu() {}
-
-export default IngresoProductos;
