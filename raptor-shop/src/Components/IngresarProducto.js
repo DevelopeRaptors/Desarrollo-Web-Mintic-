981de653f7b5db7dcebcde300b03819223828397
Aura Velez <auraveleze@gmail.com>
2022-11-02 23:17:43 -0500

    Componente IngresarProductos

--- raptor-shop/src/Components/IngresarProducto.js
+++ raptor-shop/src/Components/IngresarProducto.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import producto from '../Img/icono-producto.jpg';
+import '../Styles/IngresarProducto.css';
 
 const IngresarProducto = () => {
     return (
         <div>
-            <p>Ingresar</p>
+            <div class="container">
+                <div class="productos">
+                    <p class="listaproductos">Producto 1</p>
+                    <p class="listaproductos">Producto 2</p>
+                    <p class="listaproductos">Producto 3</p>
+
+                    <button class="btn">Agregar producto</button>
+                </div>
+
+                <div class="informacion">
+                    <img class="imagen" src={producto}></img>
+                    
+                    <form class="formulario">
+
+                        <label class="titulo-info">Nombre</label>
+                        <input type="text" class="cajon-info"></input>
+
+                        <label class="titulo-info">Descripción</label>
+                        <textarea type="text" class="area-info"></textarea>
+
+                        <label class="titulo-info">Precio</label>
+                        <input type="text" class="cajon-info"></input>
+
+                        <label class="titulo-info">Stock</label>
+                        <input type="text" class="cajon-info"></input>
+
+                        <button class="btn">Guardar información</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
